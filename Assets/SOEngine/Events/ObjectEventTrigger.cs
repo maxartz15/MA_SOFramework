84634af2dd2c5afef324839d8e8bae0530ba9b48
max <max.mxmail@gmail.com>
2019-04-28 17:40:01 +0200

    Updated example scene.
    updated example scene, reverted health.cs, updated object event trigger.

--- Assets/SOEngine/Events/ObjectEventTrigger.cs
+++ Assets/SOEngine/Events/ObjectEventTrigger.cs
@@ -1,18 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using SOEngine.Base;
 
 namespace SOEngine
 {
 	public class ObjectEventTrigger : MonoBehaviour
 	{
 		[Tooltip("Event to trigger.")]
-		public eventType m_eventType;
-		public float m_test;
+		public eventObject m_event;
 
 		public void TriggerEvent()
 		{
-			m_eventType.InvokeEvent();
+			m_event.InvokeEvent();
 		}
 	}
 }
--- Assets/SOEngine/Examples/Health.cs
+++ Assets/SOEngine/Examples/Health.cs
@@ -7,31 +7,6 @@ using SOEngine.Base;
 public class Health : MonoBehaviour
 {
 	public floatType health = null;
-	public floatType health1 = null;
-	public float faesoifhase123jlfh;
-	public floatType health2 = null;
-	public float faesoifhasejlfh;
-
-	public floatObject newHealh = null;
-
-	public floatObject[] dammfloats;
-
-	public floatType[] dammdsdfloats;
-
-	public List<ffffff> fasdfasdf = new List<ffffff>();
-
-	public eventType asldkfjhasdlkf = null;
-	public eventType asldkfjhasdlkf1 = null;
-	public eventType asldkfjhasdlkf2 = null;
-
-	[System.Serializable]
-	public struct ffffff
-	{
-		public floatType sdfioasheruopahf;
-		public float myfloat;
-		public floatObject asdkdj;
-	}
-
 
 	void Update()
     {
